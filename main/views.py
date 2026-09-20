06591283b1187503946b594dfb7206097d6d7066
reeejaaaaaa <rhezaabdilla0316@gmail.com>
2026-09-20 23:27:45 +0700

    Ubah show_project jd JSON deseriali ze

--- main/views.py
+++ main/views.py
@@ -57,17 +57,20 @@ def create_project(request):
     return render(request, "projects_form.html", context)
 
 def show_projects(request):
+    json_response = get_projects_json(request)
+
+    projects = serializers.deserialize(
+        "json",
+        json_response.content.decode("utf-8"),
+    )
+
+    projects = [project.object for project in projects]
+
     title_query = request.GET.get("title", "").strip()
 
-    project_list = Project.objects.all()
-
-    if title_query:
-        project_list = project_list.filter(
-            title__icontains=title_query
-        )
-
     context = {
-        "project_list": project_list,
+        "name": "Rheza Abdilla",
+        "project_list": projects,
         "title_query": title_query,
     }
 
